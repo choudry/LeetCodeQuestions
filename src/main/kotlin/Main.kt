1d1432b3527e1d09950ba4c097ced2140537608d
Muhammad Usman <chmusman88@gmail.com>
2022-10-19 15:44:48 +0800

    Added transform function

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+
+fun transform(input: ArrayList<Int>): ArrayList<String> {
+    val temp = ArrayList<String>()
+    for (item in input) {
+        temp.add(item.toString())
+    }
+    return  temp
+}
+
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+
+
+}
